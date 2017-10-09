7462091b02e9132b91d0e31fe0a0cb1e9b294637
Jozef Spisiak <jozef.spisiak@vacuumlabs.com>
2017-10-09 16:07:02 +0200

    Implemented improvements allowing writing only correct letters and symbols with flash notification

--- src/AwesomeIBANInput.js
+++ src/AwesomeIBANInput.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import './AwesomeIBANInput.css';
 
 const MAX_IBAN_LENGTH = 24;
+const MAX_IBAN_LETTERS = 2;
+const FLASH_DURATION = 1000;
+const ALLOWED_KEYS = ['Backspace', 'Shift', 'Delete'];
 
 class AwesomeIBANInput extends Component {
   constructor() {
     super();
-    this.handleInput = this.handleInput.bind(this);
+    this.onInput = this.onInput.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
     this.state = {
-      iban: ''
+      iban: '',
+      flash: false
     };
   }
 
-  handleInput() {
-    const value = this.ibanInput.value.replace(/[\s]/g, '');
+  onInput() {
+    const value = this.cleanIBANfromWhitespace(this.ibanInput.value);
 
     if (value.length > MAX_IBAN_LENGTH) {
       return;
@@ -24,8 +29,50 @@ class AwesomeIBANInput extends Component {
     this.setState({iban: formatedIBAN})
   }
 
+  onKeyDown(ev) {
+    const stateIBANcleaned = this.cleanIBANfromWhitespace(this.state.iban);
+
+    if (this.isLetter(ev.key)) {
+      if (stateIBANcleaned.length >= MAX_IBAN_LETTERS) {
+        ev.preventDefault();
+        this.setFlash();
+      }
+    } else if (this.isNumber(ev.key)) {
+      if (stateIBANcleaned.length < MAX_IBAN_LETTERS) {
+        ev.preventDefault();
+        this.setFlash();
+      }
+    } else if (ALLOWED_KEYS.includes(ev.key)){
+
+    } else {
+      ev.preventDefault();
+      this.setFlash();
+    }
+  }
+
+  setFlash() {
+    this.setState({flash: true});
+    setTimeout(this.unsetFlash.bind(this),FLASH_DURATION);
+  }
+
+  unsetFlash() {
+    this.setState({flash: false});
+  }
+
   formatIBAN(iban) {
     return iban.replace(/(.{4})/g, '$1 ').trim();
+  }
+
+  cleanIBANfromWhitespace(iban) {
+    return iban.replace(/[\s]/g, '');
+  }
+
+  isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
+  }
+
+  isNumber(number) {
+    return /^[0-9]$/.test(number);
   }
 
   render() {
@@ -34,7 +81,9 @@ class AwesomeIBANInput extends Component {
         Enter your IBAN here:
         <input
         name="AwesomeIBANInput"
-        onInput={this.handleInput}
+        className={this.state.flash ? 'flash' : ''}
+        onInput={this.onInput}
+        onKeyDown={this.onKeyDown}
         value={this.state.iban}
         ref={(input) => { this.ibanInput = input; }}
         />
