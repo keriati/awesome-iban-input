5d25f0b99c79328ba3dc44e38aa5ff2f7d2e54ee
Matej Mihalik <matej.mihalik@capco.com>
2018-05-22 14:33:45 +0200

    Add IbanInput component

--- awesome-iban-input/src/components/IbanInput/IbanInput.jsx
+++ awesome-iban-input/src/components/IbanInput/IbanInput.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 
 class IbanInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: ''
+    };
+
+    this.input = React.createRef();
+    this.formatInput = this.formatInput.bind(this);
+  }
+
+  formatInput(e) {
+    const value = this.input.current.value.replace(/ /g, '');
+    this.setState({
+      value: value.replace(/(.{1,4})/g, '$1 ').trim()
+    });
+  }
+
   render() {
+    const { value } = this.state;
+
     return (
-      <h1>Hello iban input</h1>
+      <input type="text"
+             value={value}
+             maxLength="29"
+             ref={this.input}
+             onChange={this.formatInput}
+      />
     );
   }
 }
